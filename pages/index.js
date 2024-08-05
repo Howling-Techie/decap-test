70daef2c8dc402ad309ae4dec79cb55ba7201ce3
Howling Techie <howling.techie@gmail.com>
2024-08-05 12:27:57 +0100

    Second attempt to add tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import { Component } from 'react'
 import { attributes, react as HomeContent } from '../content/home.md'
+import "../globals.css"
 
 export default class Home extends Component {
     render() {
